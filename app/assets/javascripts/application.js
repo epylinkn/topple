7bac79fae72234dd351b08651e0badc5ff9c929c
Anthony Bui <anthonybui@gmail.com>
2014-05-17 09:22:25 -0700

    Reconfigure bower to be at `vendor/assets/components`
    * Rails 4 automatically adds everything after /assets to the asset path!
    * Bower defaults are fucking hideous.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,8 +1,9 @@
 //= require jquery
 //= require jquery_ujs
-//= require bower_components/angular/angular
-//= require bower_components/angular-route/angular-route
-//= require bower_components/firebase/firebase
-//= require bower_components/firebase-simple-login/firebase-simple-login
-//= require bower_components/bootstrap/dist/js/bootstrap.min
+//= require angular/angular
+//= require angular-route/angular-route
+//= require angular-resource/angular-resource
+//= require firebase/firebase
+//= require firebase-simple-login/firebase-simple-login
+//= require bootstrap/dist/js/bootstrap.min
 //= require_tree .
